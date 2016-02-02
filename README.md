e800b4ebaec9a08baf7c4533ee02f2523ffa2efd
Karan Goel <karan@goel.io>
2016-02-01 21:09:21 -0800

    typos and cleanup

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ![](icon-144.png)
 
-We Know You're Just Going To Google This Headline Antway, So Here Is A Link To The Free Article, You Cheap Bastard
+We Know You're Just Going To Google This Headline Anyway, So Here Is A Link To The Free Article, You Cheap Bastard
 
 ![https://chrome.google.com/webstore/detail/fbnkddhhmgncpdelhcefpeebnjeoicbl](http://i.imgur.com/CzUM4CB.png)
 
--- script.js
+++ script.js
@@ -2,15 +2,9 @@ titleEle = document.getElementsByClassName('wsj-article-headline')[0];
 subHeadEle = document.getElementsByClassName('sub-head')[0];
 title = titleEle.textContent;
 
-// var a = document.createElement('a');
-// var linkText = document.createTextNode("We Know You're Just Going To Google This Headline Antway, So Here Is A Link To The Free Article, You Cheap Bastard");
-// a.appendChild(linkText);
-// a.title = "We Know You're Just Going To Google This Headline Antway, So Here Is A Link To The Free Article, You Cheap Bastard";
-// a.href = "http://example.com";
-
 var buttonnode = document.createElement('input');
 buttonnode.setAttribute('type', 'button');
-buttonnode.setAttribute('value', "We Know You're Just Going To Google This Headline Antway, So Here Is A Link To The Free Article, You Cheap Bastard");
+buttonnode.setAttribute('value', "We Know You're Just Going To Google This Headline Anyway, So Here Is A Link To The Free Article, You Cheap Bastard");
 buttonnode.onclick = function() {
   window.open('http://www.google.com/search?q=' + title, '_blank');
 };
